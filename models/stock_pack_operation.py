978ea237da90ac193716fc440d0d74c635fc4595
Marco Cid <marcocb46@gmail.com>
2018-01-23 22:30:32 -0600

    correcciones

--- models/stock_pack_operation.py
+++ models/stock_pack_operation.py
@@ -1,4 +1,64 @@
-from odoo import api, exceptions, fields, models
+from odoo import api, exceptions, fields, models, _
+from odoo.tools.float_utils import float_compare, float_round, float_is_zero
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+import time
+
+
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+
+
+
+    def check_backorder(self):
+        #print 'check_backorder----------------'
+        need_rereserve, all_op_processed = self.picking_recompute_remaining_quantities(done_qtys=True)
+
+        #SE OBTIENE PRIMER PICKING
+        #first_picking = self.get_first_picking(self.group_id)
+
+        for move in self.move_lines:
+            #if not move.ignore:
+            if move.state != 'cancel':
+            #if not self.ignore(move,first_picking): #SOLO TOMAR EN CUENTA SI NO ES DE VISCOSIDAD
+                if float_compare(move.remaining_qty, 0, precision_rounding=move.product_id.uom_id.rounding) != 0:
+                    return True
+        return False
+
+    # def ignore(self,move,first_picking):
+    #     """
+    #     REVISA SI EL MOVIMIENTO ES DE VISCOSIDAD
+    #     SI REGRESA TRUE ES QUE SI
+    #     """
+    #     print 'ignore'
+    #     if first_picking:
+    #         if first_picking.picking_type_code == 'incoming':
+    #             for operation in first_picking.pack_operation_ids:
+    #                 if operation.viscosity and operation.viscosity > 0:
+    #                     if operation.product_id.id == move.product_id.id:
+    #                         print 'move.product_qty: ',move.product_qty
+    #                         print 'abs(operation.ordered_qty-operation.product_qty)): ',abs(operation.ordered_qty-operation.product_qty)
+    #                         # if float_compare(move.product_qty, abs(operation.ordered_qty-operation.product_qty), \
+    #                         # precision_rounding=move.product_id.uom_id.rounding) != 0:
+    #                         if move.product_qty == (abs(operation.ordered_qty-operation.product_qty)):
+    #                             print 'TRUE'
+    #                             return True
+    #     return False
+
+    # def get_first_picking(self,group_id):
+    #     print 'get_first_picking'
+    #     if group_id:
+    #         group_id = group_id.id
+    #         move_obj = self.env['stock.move']
+    #         moves = move_obj.search([('group_id','=',group_id)])
+    #         print 'moves: ',moves
+    #         if moves:
+    #             picking_ids = [move.picking_id.id for move in moves]
+    #             picking_ids.sort()
+    #             print 'picking_ids: ',picking_ids
+    #             first_pick_id = picking_ids[0]
+    #             first_picking = [move.picking_id for move in moves if first_pick_id == move.picking_id.id][0]
+    #             return first_picking
+    #     return False
 
 
 class StopckPackOperation(models.Model):
@@ -67,3 +127,10 @@ class StopckPackOperation(models.Model):
         for r in self:
             if r.picking_id and r.picking_id.picking_type_id:
                 r.picking_type_code = r.picking_id.picking_type_id.code
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    #INDICA SI EL MOV SERA IGNORADO AL CALCULAR LA CANTIDAD FALTANTE (remaining_qty)
+    ignore = fields.Boolean()
